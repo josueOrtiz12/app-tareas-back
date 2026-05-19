98ccc2af50a8dc137c4749b99c68b9db8e2e8cfb
Josue O <ortizjd0036@gmail.com>
2026-05-19 17:24:38 -0400

    feature[comentarios finales archivo README]

--- src/helpers/authHelper.test.js
+++ src/helpers/authHelper.test.js
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "vitest";
+  import { describe, it, expect } from "vitest";
 import { hashPassword, comparePassword, verifyToken, generateToken } from "./authHelper.js";
 
 describe("authHelper", () => {
